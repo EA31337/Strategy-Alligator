76c6791f37a6c6c71ebd265d12835a06218d6412
kenorb <kenorb@users.noreply.github.com>
2020-12-31 23:40:34 +0000

    config: Adds applied_price and ma_method

--- config/EURUSD_H8.h
+++ config/EURUSD_H8.h
@@ -6,12 +6,14 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_Alligator_Params_H8 : Indi_Alligator_Params {
   Indi_Alligator_Params_H8() : Indi_Alligator_Params(indi_alli_defaults, PERIOD_H8) {
+    applied_price = (ENUM_APPLIED_PRICE)0;
     jaw_period = 30;
     jaw_shift = 0;
-    teeth_period = 11;
-    teeth_shift = 0;
+    teeth_period = 10;
+    teeth_shift = 11;
     lips_period = 34;
     lips_shift = 0;
+    ma_method = 2;
     shift = 0;
   }
 } indi_alli_h8;
